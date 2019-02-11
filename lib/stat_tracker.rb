deeb52d6d3ae80fa095192be9bb8376b6c3f9f9d
Zach Leach <zachleach@techoverco.com>
2019-02-11 16:40:17 -0700

    Added test for from_csv method and verified that it worked.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,7 +1,16 @@
+require 'csv'
+
 class StatTracker
 
-  def self.from_csv
-    StatTracker.new
+  def initialize(data)
+    @data = data
   end
 
+  def self.from_csv(files)
+    options = {headers: true}
+    StatTracker.new({
+        games: CSV.open(files[:games], options),
+        teams: CSV.open(files[:teams], options),
+        game_teams: CSV.open(files[:game_teams], options)})
+  end
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,11 +1,29 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/stat_tracker'
+require 'pry'
 
 class StatTrackerTest < Minitest::Test
 
   def setup
-    @stat_tracker = StatTracker.new
+    @stat_tracker = StatTracker.new(nil)
+    @team_info_arr = [
+                    ["team_id","franchiseId","shortName","teamName","abbreviation","link"],
+                    ["1","23","New Jersey","Devils","NJD","/api/v1/teams/1"],
+                    ["4","16","Philadelphia","Flyers","PHI","/api/v1/teams/4"],
+                    ["26","14","Los Angeles","Kings","LAK","/api/v1/teams/26"],
+                  ]
+
+
+    @game_path = './data/game.csv'
+    @team_path = './data/team_info.csv'
+    @game_teams_path = './data/game_teams_stats.csv'
+
+    @locations = {
+      games: @game_path,
+      teams: @team_path,
+      game_teams: @game_teams_path
+  }
   end
 
   def test_stat_tracker_exists
@@ -13,8 +31,6 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_stat_tracker_has_from_csv_method
-    assert_instance_of StatTracker, StatTracker.from_csv
+    assert_instance_of StatTracker, StatTracker.from_csv(@locations)
   end
-
-
 end
