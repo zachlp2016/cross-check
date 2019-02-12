8bafcb3e7008bc0439c22a8cd3fc2aa65fa97fcf
Jon Peterson <jon@pnwebs.com>
2019-02-12 15:10:58 -0700

    Fix bug in how Game class was accessing values from CSV.

--- test/game_test.rb
+++ test/game_test.rb
@@ -18,21 +18,21 @@ class GameTest < Minitest::Test
     # @games = Game.new(@stat_tracker.games)
 
     @game_data = {
-          game_id: "2012030221",
-          season: "20122013",
-          type: "P",
-          date_time: 2013-05-16,
-          away_team_id: "3",
-          home_team_id: "6",
-          away_goals: 2,
-          home_goals: 3,
-          outcome: "home win OT",
-          home_rink_side_start: "left",
-          venue: "TD Garden",
-          venue_link: "/api/v1/venues/null",
-          venue_time_zone_id: "America/New_York",
-          venue_time_zone_offset: -4,
-          venue_time_zone_tz: "EDT"
+          "game_id" => "2012030221",
+          "season" => "20122013",
+          "type" => "P",
+          "date_time" => 2013-05-16,
+          "away_team_id" => "3",
+          "home_team_id" => "6",
+          "away_goals" => 2,
+          "home_goals" => 3,
+          "outcome" => "home win OT",
+          "home_rink_side_start" => "left",
+          "venue" => "TD Garden",
+          "venue_link" => "/api/v1/venues/null",
+          "venue_time_zone_id" => "America/New_York",
+          "venue_time_zone_offset" => -4,
+          "venue_time_zone_tz" => "EDT"
         }
 
     @game = Game.new(@game_data)
