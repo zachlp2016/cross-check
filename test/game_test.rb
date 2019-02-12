1de7128b091f510ef1c073bcd9a328042197afd7
Zach Leach <zachleach@techoverco.com>
2019-02-11 18:09:25 -0700

    Created data instance variables and tested that they can be retrieved from the game class.

--- test/game_test.rb
+++ test/game_test.rb
@@ -43,7 +43,22 @@ class GameTest < Minitest::Test
   end
 
   def test_games_hold_game_data
-    assert_equal @game_data, @game.data
+    assert_equal "2012030221", @game.game_id
+    assert_equal "20122013", @game.season
+    assert_equal "P", @game.type
+    assert_equal 2013-05-16, @game.date_time
+    assert_equal "3", @game.away_team_id
+    assert_equal "6", @game.home_team_id
+    assert_equal 2, @game.away_goals
+    assert_equal 3, @game.home_goals
+    assert_equal "home win OT", @game.outcome
+    assert_equal "left", @game.home_rink_side_start
+    assert_equal "TD Garden", @game.venue
+    assert_equal "/api/v1/venues/null", @game.venue_link
+    assert_equal "America/New_York", @game.venue_time_zone_id
+    assert_equal -4, @game.venue_time_zone_offset
+    assert_equal "EDT", @game.venue_time_zone_tz
+
   end
 
 end
