6956c4a8c5d469157d9c01cdfc443ff62027937a
Jon Peterson <jon@pnwebs.com>
2019-02-11 18:15:22 -0700

    Change team_details keys to strings to match tests.

--- lib/team.rb
+++ lib/team.rb
@@ -6,11 +6,11 @@ class Team
               :abbreviation,
               :link
   def initialize(team_details)
-    @team_id = team_details[:team_id]
-    @franchise_id = team_details[:franchiseId]
-    @short_name = team_details[:shortName]
-    @team_name = team_details[:teamName]
-    @abbreviation = team_details[:abbreviation]
-    @link = team_details[:link]
+    @team_id = team_details["team_id"]
+    @franchise_id = team_details["franchiseId"]
+    @short_name = team_details["shortName"]
+    @team_name = team_details["teamName"]
+    @abbreviation = team_details["abbreviation"]
+    @link = team_details["link"]
   end
 end
