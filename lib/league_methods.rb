412ce0c3b741c3cb23cc126115b9ffbbef603007
Jon Peterson <jon@pnwebs.com>
2019-02-12 14:36:43 -0700

    Add favorite_opponent method, refactor out shared logic.

--- lib/league_methods.rb
+++ lib/league_methods.rb
@@ -1,8 +1,12 @@
 module LeagueMethods
-  def team_info(team_id)
+  def get_team(team_id)
     @teams.find do |team|
       team.team_id == team_id
-    end.info
+    end
+  end
+
+  def team_info(team_id)
+    get_team(team_id).info
   end
 
   def get_games_by_team(team_id)
@@ -51,5 +55,24 @@ module LeagueMethods
   def fewest_goals_scored(team_id)
     games = get_games_by_team(team_id)
     games.min_by{|game| game.goals}.goals
+  end
+
+  def favorite_opponent(team_id)
+    games = get_games_by_team(team_id)
+    outcomes_against = Hash.new{|hash,opponent|
+      hash[opponent] = {win: 0, loss: 0}
+    }
+    games.each do |target_game|
+      opponent = @game_teams.find do |game|
+        game.game_id == target_game.game_id &&
+        game.team_id != target_game.team_id
+      end.team_id
+      outcome = target_game.won ? :win : :loss
+      outcomes_against[opponent][outcome] += 1
+    end
+    favorite_id = outcomes_against.max_by do |opponent,stats|
+      stats[:win].to_f / stats[:loss]
+    end[0]
+    get_team(favorite_id).team_name
   end
 end
