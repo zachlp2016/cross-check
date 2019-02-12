e7d4ada01d1775014f8a571f741721d4011383b9
Jon Peterson <jon@pnwebs.com>
2019-02-11 17:48:07 -0700

    Corrected initial test (should be unit test, not integration)

--- test/team_test.rb
+++ test/team_test.rb
@@ -6,15 +6,23 @@ require 'pry'
 
 class TeamTest < Minitest::Test
   def setup
-    @files = {
-      games: './data/game_test.csv',
-      teams: './data/team_info.csv',
-      game_teams: './data/game_teams_stats_test.csv'
-    }
-    @stat_tracker = StatTracker.from_csv(@files)
+    # @files = {
+    #   games: './data/game_test.csv',
+    #   teams: './data/team_info.csv',
+    #   game_teams: './data/game_teams_stats_test.csv'
+    # }
+    # @stat_tracker = StatTracker.from_csv(@files)
+
+    @team = Team.new({
+      team_id: 1,
+      franchiseId: 23,
+      shortName: "New Jersey",
+      teamName: "Devils",
+      abbreviation: "NJD",
+      link: "/api/v1/teams/1"})
   end
 
   def test_team_exists
-    assert_instance_of Team, @stat_tracker.teams[0]
+    assert_instance_of Team, @team
   end
 end
