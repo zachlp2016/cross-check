3d6f0a5cb183c81568291b3908e40250780f20ab
Zach Leach <zachleach@techoverco.com>
2019-02-14 00:11:26 -0700

    Started working on the best_defense method.

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -10,6 +10,12 @@ module LeagueStatistics
     end
   end
 
+  def group_by_game
+    games = @game_teams.group_by do |game_team|
+      game_team.game_id
+    end
+  end
+
   def best_offense
     max_team_average_by_id = group_by_team.max_by do |team|
       sum_of_team_goals = team[1].sum do |game|
@@ -36,5 +42,17 @@ module LeagueStatistics
         return team.short_name
       end
     end
+  end
+
+  def best_defense
+    goals_allowed_by_team = {}
+    group_by_game.each do |game|
+      if game[1][0].home_or_away == "home"
+        goals_allowed_by_team[game[1][0].team_id] = game[1][1].goals
+      elsif game[1][1].home_or_away == "away"
+        goals_allowed_by_team[game[1][1].team_id] = game[1][0].goals
+      end
+    end
+    binding.pry
   end
 end
--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -5,7 +5,6 @@ require './lib/game_team'
 require './lib/game'
 require './lib/stat_tracker'
 require './lib/league_statistics'
-
 require 'pry'
 
 class LeagueStatisticsTest < Minitest::Test
@@ -14,27 +13,26 @@ class LeagueStatisticsTest < Minitest::Test
 
       @game_path = './data/game_test.csv'
       @team_path = './data/team_info.csv'
-      @game_teams_path = './data/game_teams_stats.csv'
+      @game_team_path = './data/game_teams_stats.csv'
 
-      @game_teams_test_path = './data/test/game_teams_league_stats_test.csv'
-      @games_test_path = './data/test/game_league_stats_test.csv'
+      @game_team_test_path = './data/test/game_teams_league_stats_test.csv'
+      @game_test_path = './data/test/game_league_stats_test.csv'
+
       @locations = {
         games: @game_path,
         teams: @team_path,
-        game_teams: @game_teams_path
+        game_teams: @game_team_path
       }
-      @locations_test = {
-        games: @game_path,
-        teams: @team_path,
-        game_teams: @game_teams_test_path
-      }
-
 
       @stat_tracker = StatTracker.from_csv(@locations)
 
-      @stat_tracker_test_games_teams = StatTracker.from_csv(@game_teams_data)
-      @stat_tracker_test_games = StatTracker.from_csv(@games_data)
+      @test_data = {
+        games: @game_test_path,
+        teams: @team_path,
+        game_teams: @game_team_test_path
+      }
 
+      @test_stat_tracker = StatTracker.from_csv(@test_data)
 
   end
 
@@ -45,12 +43,21 @@ class LeagueStatisticsTest < Minitest::Test
   end
 
   def test_method_best_offense_works
-    assert_equal "Boston", @stat_tracker_test.best_offense
+    assert_equal "Boston", @test_stat_tracker.best_offense
     assert_equal "Vegas", @stat_tracker.best_offense
   end
 
   def test_method_worst_offense_works
-    assert_equal "Pittsburgh", @stat_tracker_test.worst_offense
+    assert_equal "Pittsburgh", @test_stat_tracker.worst_offense
     assert_equal "Buffalo", @stat_tracker.worst_offense
+  end
+
+  def test_method_best_defense_works
+    assert_equal "Boston", @test_stat_tracker.best_defense
+  end
+
+  def test_method_worst_defense_works
+    skip
+    assert_equal "NY Rangers", @test_stat_tracker.worst_defense
   end
 end
