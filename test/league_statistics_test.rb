99a995c0b6ef1c151477ad51d3b0c70c0dc1d3b2
Zach Leach <zachleach@techoverco.com>
2019-02-13 22:19:33 -0700

    Added new folders for test csv data for league stats and retested both best offense and worst offense with the new 30 line data.

--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -14,24 +14,43 @@ class LeagueStatisticsTest < Minitest::Test
 
       @game_path = './data/game_test.csv'
       @team_path = './data/team_info.csv'
-      @game_teams_path = './data/game_teams_stats_test.csv'
+      @game_teams_path = './data/game_teams_stats.csv'
+
+      @game_teams_test_path = './data/test/game_teams_league_stats_test.csv'
+      @games_test_path = './data/test/game_league_stats_test.csv'
       @locations = {
         games: @game_path,
         teams: @team_path,
         game_teams: @game_teams_path
       }
+      @locations_test = {
+        games: @game_path,
+        teams: @team_path,
+        game_teams: @game_teams_test_path
+      }
+
+
       @stat_tracker = StatTracker.from_csv(@locations)
+
+      @stat_tracker_test_games_teams = StatTracker.from_csv(@game_teams_data)
+      @stat_tracker_test_games = StatTracker.from_csv(@games_data)
+
+
   end
+
+
 
   def test_method_count_of_teams
     assert_equal 33, @stat_tracker.count_of_teams
   end
 
   def test_method_best_offense_works
-    assert_equal "Pittsburgh", @stat_tracker.best_offense
+    assert_equal "Boston", @stat_tracker_test.best_offense
+    assert_equal "Vegas", @stat_tracker.best_offense
   end
 
-  def test_method_best_defense_works
-    assert_equal "Philadelphia", @stat_tracker.worst_offense
+  def test_method_worst_offense_works
+    assert_equal "Pittsburgh", @stat_tracker_test.worst_offense
+    assert_equal "Buffalo", @stat_tracker.worst_offense
   end
 end
