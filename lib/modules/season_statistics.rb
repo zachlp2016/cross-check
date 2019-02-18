81b89e4bc6eee33c5d7029667d6220f3e519e8cd
Jon Peterson <jon@pnwebs.com>
2019-02-18 09:12:39 -0700

    Implemented most/least hits.

--- lib/modules/season_statistics.rb
+++ lib/modules/season_statistics.rb
@@ -10,4 +10,16 @@ module SeasonStatistics
     team = team_stats.min_by{|team,stats| stats[:goals].to_f / stats[:shots]}[0]
     return get_team_name(team)
   end
+
+  def most_hits(season_id)
+    team_stats = get_team_stats_for_single_season(season_id)
+    team = team_stats.max_by{|team,stats| stats[:hits]}[0]
+    return get_team_name(team)
+  end
+
+  def least_hits(season_id)
+    team_stats = get_team_stats_for_single_season(season_id)
+    team = team_stats.min_by{|team,stats| stats[:hits]}[0]
+    return get_team_name(team)
+  end
 end
