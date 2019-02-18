8c4f9bf438bbf133c9645fab31863031c8bc7e7c
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-18 15:40:04 -0700

    editing game_methods test

--- test/game_methods_test.rb
+++ test/game_methods_test.rb
@@ -6,9 +6,9 @@ require 'pry'
 class GameMethodsTest < Minitest::Test
 
   def setup
-    @game_path = './data/game.csv'
+    @game_path = './test/data/game_test.csv'
     @team_path = './data/team_info.csv'
-    @game_teams_path = './data/game_teams_stats.csv'
+    @game_teams_path = './test/data/game_teams_stats_test.csv'
     @locations = {
       games: @game_path,
       teams: @team_path,
@@ -19,7 +19,7 @@ class GameMethodsTest < Minitest::Test
 
   def test_highest_total_score
 
-    assert_equal 15, @stat_tracker.highest_total_score
+    assert_equal 13, @stat_tracker.highest_total_score
   end
 
   def test_lowest_total_score
@@ -29,34 +29,34 @@ class GameMethodsTest < Minitest::Test
 
   def test_biggest_blowout
 
-    assert_equal 10, @stat_tracker.biggest_blowout
+    assert_equal 7, @stat_tracker.biggest_blowout
   end
 
   def test_home_and_away_games_won
 
-    assert_equal 4089 , @stat_tracker.total_home_games_won
-    assert_equal  3352, @stat_tracker.total_away_games_won
+    assert_equal 299 , @stat_tracker.total_home_games_won
+    assert_equal  231, @stat_tracker.total_away_games_won
   end
 
   def test_total_games_played
 
-    assert_equal 7441.0, @stat_tracker.total_games
+    assert_equal 530.0, @stat_tracker.total_games
   end
 
   def test_percentage_home_and_away_wins
 
-    assert_equal 0.55, @stat_tracker.percentage_home_wins
-    assert_equal 0.45, @stat_tracker.percentage_visitor_wins
+    assert_equal 0.56, @stat_tracker.percentage_home_wins
+    assert_equal 0.44, @stat_tracker.percentage_visitor_wins
   end
 
   def test_count_of_games_by_season
     expected = {
-      "20122013"=>806,
-      "20162017"=>1317,
-      "20142015"=>1319,
-      "20152016"=>1321,
-      "20132014"=>1323,
-      "20172018"=>1355
+      "20122013"=>86,
+      "20162017"=>87,
+      "20142015"=>89,
+      "20152016"=>91,
+      "20132014"=>93,
+      "20172018"=>84
     }
 
     assert_equal expected, @stat_tracker.count_of_games_by_season
@@ -64,17 +64,17 @@ class GameMethodsTest < Minitest::Test
 
   def test_average_goals_per_game
 
-    assert_equal 5.54, @stat_tracker.average_goals_per_game
+    assert_equal 5.33, @stat_tracker.average_goals_per_game
   end
 
   def test_average_goals_by_season
     expected = {
-      "20122013"=>5.4,
-      "20162017"=>5.51,
-      "20142015"=>5.43,
-      "20152016"=>5.41,
-      "20132014"=>5.5,
-      "20172018"=>5.94
+      "20122013"=>5.02,
+      "20162017"=>5.16,
+      "20142015"=>5.03,
+      "20152016"=>5.25,
+      "20132014"=>5.59,
+      "20172018"=>5.9
     }
 
     assert_equal expected, @stat_tracker.average_goals_by_season
